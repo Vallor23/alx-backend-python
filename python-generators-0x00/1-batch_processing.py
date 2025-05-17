35cad0df152d242f1e99d5b2bf26d64f7d96859a
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-17 18:31:09 +0300

    Create a generator to fetch and process data in batches from the users database

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,29 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database="ALX_prodev"
+    )
+    
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    
+    batch = []
+    for row in cursor:
+        batch.append(row)
+        
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches:
+        filtered = (user for user in batch if int(user['age'])> 25)
+        yield batch
