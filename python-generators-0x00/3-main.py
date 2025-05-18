30f69c90008c48ad4b5626950f068f3632583cd4
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-18 08:25:26 +0300

    Add print to test out the generator function

--- python-generators-0x00/3-main.py
+++ python-generators-0x00/3-main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import sys
-lazy_paginator = __import__('2-lazy_paginate').lazy_pagination
+lazy_paginator = __import__('2-lazy_paginate').lazy_paginate
 
 
 try:
+    page_num = 1
     for page in lazy_paginator(100):
+        print(f"Page {page_num}:")
         for user in page:
             print(user)
-
+        print("--------")
+        page_num += 1
 except BrokenPipeError:
     sys.stderr.close()
