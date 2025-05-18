5a7aeb3e6ccd9b0d4af4ca3d898b93cd412d8758
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-18 17:56:44 +0300

    Create a generator to compute a memory-efficient aggregate function

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def  stream_user_ages():
     connection = mysql.connector.connect(
@@ -9,3 +11,27 @@ def  stream_user_ages():
         password=os.getenv("MYSQL_PASSWORD"),
         database="ALX_prodev"
     )
+    
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT age FROM user_data")
+    
+    user_ages =[]
+    for row in cursor:
+        yield(row['age'])
+        
+    cursor.close()
+    connection.close()
+    
+def average_user_age():
+    total_age = 0
+    count = 0
+    for age in stream_user_ages():
+        total_age += age
+        count += 1
+        
+    if count == 0:
+        return 0
+    
+    return total_age / count
+    
+print(f"Average age of users: {average_user_age()}")
