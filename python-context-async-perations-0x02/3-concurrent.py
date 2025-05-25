14e92a3184b91701f3a41018ca627aaa9cae6998
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-25 18:49:13 +0300

    Run multiple database queries concurrently using asyncio.gather

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,33 @@
+import aiosqlite
+import asyncio
+import os
+# from dotenv  import load_dotenv
+
+# load_dotenv()
+
+async def  async_fetch_users():
+    async with aiosqlite.connect("user_db") as db:
+        db.row_factory = aiosqlite.Row #Allows accessing colums as rows
+        async with db.execute("SELECT * FROM users") as cursor:
+            rows = await cursor.fetchall()
+            return [(row["id"], row["name"],row["age"]) for row in rows]
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect("user_db") as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            rows = await cursor.fetchall()
+            return [(row["id"], row["name"],row["age"]) for row in rows]
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+    
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently()) 
+        
+
+
+       
