346ba08bbc8585040768e798a10ae01b396db0db
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-31 16:20:54 +0300

    build serializers for the models

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User, Messages, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["user_id", "first_name", "last_name","email", "password", "phone_number", "profile_picture", "is_online", "last_seen"]
+        
+class MessagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["message_id", "sender", "message_body","recipient", "sent_at", "conversation"]
+
+class ConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["conversation_id", "participants", "created_at","updated_at", "sent_at", "conversation"]
