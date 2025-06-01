7714e44db79bd7108e8ec5418f3c9a21631a2eae
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-01 10:44:09 +0300

    Create viewset for listing messages- Fiilter by conversation_id query parameter

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -8,11 +8,12 @@ from .serializers import MessageSerializer, ConversationSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     """
     A viewset for listing conversations
+    Filters by user_id query parameter or defaults to the authenticated user's conversations.
     """
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def get_queryset(self):
         """
         Filter conversations by user_id from query parameters or the authenticated user.
@@ -30,6 +31,27 @@ class ConversationViewSet(viewsets.ModelViewSet):
                 raise ValidationError("Invalid user_id: must be an integer.")
         #Optionally returnconversations based on the authenticated user
         return Conversation.objects.filter(participants = self.request.user)
-    
 
-# class MessageViewSet:
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for listing messages
+    Filters by conversation_id query parameter or defaults to messages in the authenticated user's conversations.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.request.query_params("conversation_id", None)
+        if conversation_id is not None:
+            try:
+                conversation_id = int(conversation_id)
+                # Check if the user is a participant in the conversation
+                if not Conversation.objects.filter(id=conversation_id, participants__id = self.request.user).exists():
+                    raise ValidationError("You are not a participant of this conversation")
+                return Message.objects.filter(conversation__id = conversation_id)
+            except ValueError:
+                raise ValidationError("Invalid conversation_id: must be an integer.")
+        # Default: return messages from all conversations the user is part of
+        return(Message.objects.filter(conversation__participant = self.request.user))
