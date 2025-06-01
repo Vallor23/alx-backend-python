3135dc2a83ba27fe6ece112aa7dcb52a4b89d852
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-01 20:19:05 +0300

    Chore: Include  api-auth in project urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls', namespace='chats')),
-    path('api/auth', include('rest_frameworks.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_frameworks.urls', namespace='rest_framework')),
 ]
