f0646900098ba8a8b1c91f718774b44c6f4c5eb1
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-04 19:12:48 +0300

    Include nested_router.urls in url patterns, fix imports

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,18 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
-from rest_framework_nested import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 from chats.views import ConversationViewSet, MessageViewSet
 
 # Base router for conversations
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
 # Nested router for messages under conversation
-nested_router = routers.NestedDefaultRouter(router, r'conversations', lookup='converstaion')
-nested_router.register(r'messages', MessageViewSet, basename='message')
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 app_name = 'chats'
 
-url_patterns = [
-    path('', include(router.urls))
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
