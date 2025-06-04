d277737830496d3714874baafce28787207c2c6a
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-04 19:10:12 +0300

    Fix typo in the Message model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -21,7 +21,7 @@ class Message(models.Model):
     message_id = models.UUIDField(primary_key= True, default= uuid.uuid4, editable= False)
     sender = models.ForeignKey('User', on_delete= models.CASCADE, related_name= 'sent_messages')
     receiver = models.ForeignKey('User', on_delete= models.CASCADE, related_name= 'received_messages')
-    message_body = models.CharField(200)
+    message_body = models.CharField(max_length=200)
     sent_at = models.DateTimeField(auto_now_add= True)
     conversation = models.ForeignKey('Conversation', on_delete= models.CASCADE, related_name= 'conversations')
 
