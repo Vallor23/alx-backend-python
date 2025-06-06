adb46ba60ab9630e9e24a4bea0b1be04ab9fe79e
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-06 05:37:09 +0300

    Implement a middleware that restricts access to the messaging up during certain hours of the day

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
 import datetime
 import logging
+from django.http import HttpResponseForbidden
+from django.utils import timezone
 
 logger = logging.getLogger(__name__)  # get a logger for this module
 
@@ -19,7 +21,20 @@ class OffensiveLanguageMiddleware:
     pass
 
 class RestrictAccessByTimeMiddleware:
-     def __init__(self):
-         pass
-     def __call__(self, *args, **kwds):
-         pass
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.start_hour = 9
+        self.end_hour = 18
+        
+    def __call__(self, request):
+        current_time = timezone.now()
+        current_hour = current_time.hour
+         
+        if not (self.start_hour <= current_hour < self.end_hour):
+            return HttpResponseForbidden("Access is allowed only between 9 AM and 5 PM server time. Please try again later.")
+         
+        # If within the allowed time, proceed to the next middleware or vie
+        response = self.get_response(request)
+        return response
+          
+         
