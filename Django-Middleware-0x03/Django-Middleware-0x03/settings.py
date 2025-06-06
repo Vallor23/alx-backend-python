9f5a78b5944881f1cd855b09d2c4856afd8e1a81
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-06 12:11:41 +0300

    Configure RolepermissionMiddleware

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -55,8 +55,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
-    'chats.middleware.OffensiveLanguageMiddleware'
-    
+    'chats.middleware.OffensiveLanguageMiddleware',
+    'chats.middleware.RolepermissionMiddleware', 
 ]
 
 ROOT_URLCONF = 'Django-Middleware-0x03.urls'
