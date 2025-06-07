d721977e3712e10a05ba651d6a2459ec7dda8489
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-07 09:08:40 +0300

    Register Message model

--- Django-Middleware-0x03/chats/admin.py
+++ Django-Middleware-0x03/chats/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-admin.register('Message')
+
+@admin.register('Message')
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['user_id','username','email ', 'password', 'phone_number', 's_online', 'last_seen']
+    
 admin.register('Conversation')
 admin.register('User')
