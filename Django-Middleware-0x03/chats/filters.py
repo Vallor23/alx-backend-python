d4c9b06d3def32d063c7a08a43c2cd6f2a9c896f
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-07 15:01:47 +0300

    Import django_filters rest_fremaeork

--- Django-Middleware-0x03/chats/filters.py
+++ Django-Middleware-0x03/chats/filters.py
@@ -1,8 +1,18 @@
-import django_filters
-from django.contrib.auth import get_user_model
+from .models import Message
+from django_filters import rest_framework as filters
+from django.contrib.auth.models import User
 
-User = get_user_model()
-
-class MessageFilter(django_filters.filterset):
-    #Fillter messages based on sender username
+class MessageFilter(filters.filterset):
+    #Filtering by conversation participants
+    participants = filters.ModelMultipleChoiceField(
+        field_name = 'conversation__participant',
+        query_set=User.objects.all(),
+        conjoined=True  # Requires all listed participants
+    )
+    # Filter by timestamps
+    created_at__gte = filters.DateTimeFilter(field_name='created_at', lookup_expr='gte')
+    created_at__lte = filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
     
+    class Meta:
+        model: Message
+        fields = ['participants', 'created_at__gte', 'created_at__lte']
