da1a492c6bd06235b5308e5f86c967225e2cd078
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-08 12:56:28 +0300

    Configure defaults authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -130,6 +130,8 @@ REST_FRAMEWORK = {
         'chats.permissions.IsParticipantOfConversation',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
