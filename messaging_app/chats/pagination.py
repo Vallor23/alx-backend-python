5eccb0e2cecb2312fb81a673cdf81e49548a55f6
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-08 13:52:45 +0300

    Fix typo params to param

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size =20  # Default number of messages per page
-    page_size_query_params = 'page_size'
+    page_size_query_param = 'page_size'
     max_page_size = 100
     
     def get_paginated_response(self, data):
