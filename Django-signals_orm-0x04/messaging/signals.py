e8d45855caadf9a4c5cc4002a0fce7944e10b427
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-13 19:31:12 +0300

    Create post_delete signal for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete 
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -24,3 +25,14 @@ def log_message_edits(sender, instance, **kwargs):
 
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete , sender=User)
+def clean_up(sender, instance, **kwargs):
+    instance.sent_messages.all().delete()
+    instance.received_messages.all().delete()
+    instance.notifications.all().delete()
+    # To delete all MessageHistory related to this user's messages:
+    from models import MessageHistory
+    MessageHistory.objects.filter(message_sender=instance).delete()
+    MessageHistory.objects.filter(message_receiver=instance).delete()
+    
