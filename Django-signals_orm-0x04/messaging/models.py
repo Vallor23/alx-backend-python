ae838927937e64df5230f3f598a07d77f8f2277f
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-13 18:19:06 +0300

    Add edited_by field in MessageHistory

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,7 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-
+    edited = models.BooleanField(default=False)
     def __str__(self):
         return f'Message from {self.sender} to {self.receiver} at {self.timestamp}'
     
@@ -15,7 +15,6 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
-    edited = models.BooleanField(default=False)
     notification_type = models.CharField(default='Message', max_length=50)
 
     def __str__(self):
@@ -25,6 +24,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message,on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited-by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"Edit history for {self.message.id} edited at {self.edited_at}"
