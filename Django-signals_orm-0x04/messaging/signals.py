7695f42b8ae4789327a1a85a7136411b28047fea
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-13 17:53:29 +0300

    Add  pre_save signal

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def send_notification(sender, instance, created, **kwargs):
@@ -9,3 +9,18 @@ def send_notification(sender, instance, created, **kwargs):
             user = instance.receiver,
             message = instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edits(sender, instance, **kwargs):
+    if instance.id:
+        try:
+            old_message = Message.objects.get(id=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message = old_message,
+                    old_content = old_message.content
+                )
+                instance.edited = True
+
+        except Message.DoesNotExist:
+            pass
