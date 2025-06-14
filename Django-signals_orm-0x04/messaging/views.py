a6917ac565fc85248f4fc6a93ff4d1cb9917e2f2
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-14 11:25:52 +0300

    Create et_unread_messages: Use only() in  to optimize the query to load only the is_read field

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -16,10 +16,15 @@ def get_message(request):
 
 def get_message_replies(request, pk):
     try:
-        message = Message.objects.prefetch_related('replies').get(id=pk)
+        message = Message.objects.prefetch_related('replies').get(id=pk)  # 'replies' is the related_name on the ForeignKey - parent_message
         return [
             f"Replies to '{message.content}' : {reply.content}"
             for reply in message.replies.all()
         ] 
     except Message.DoesNotExist:
         return []
+    
+def get_unread_messages(request):
+    unread_messages = Message.unread.filter(receiver=request.user).only('is_read')
+    message_ids = [{msg.id} for msg in unread_messages]
+    return f"Unread Messages : {message_ids}"
