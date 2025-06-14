3d40d1854fc47f3954043e60b9f18d4d0cc3e645
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-14 10:34:38 +0300

    Create custom manager(UnreadMessagesManager) that filters unread messages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# Custom ORM Manager for Unread messages
+class  UnreadMessagesManager(models.Manager):
+    def unread_messages(self):
+        return self.filter(is_read=True)
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
-
-    # New field to link a reply to the message it's replying to
-    parent_message = models.ForeignKey(
-        'self',   # points to *another* Message
-        null=True,
-        blank=True,
-        related_name='replies',
-        on_delete=models.CASCADE
-    )
-
-
+    is_read = models.BooleanField(default=False)
+    # New field to link a reply to the message it's replying to. 'self' points to *another* Message
+    parent_message = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    objects = UnreadMessagesManager()
+    
     def __str__(self):
         return f'Message from {self.sender} to {self.receiver} at {self.timestamp}'
-    
+
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
